19e306394e68dfb5f38568d08ac6b2d05e23959c
imsamimalik <imsamimalikk@gmail.com>
2023-08-11 22:31:19 +0500

    auto resize textarea
    Signed-off-by: imsamimalik <imsamimalikk@gmail.com>

--- components/Input.jsx
+++ components/Input.jsx
@@ -2,6 +2,9 @@
 import Dropdown from "./Dropdown";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { useContentStore } from "@/store/store";
+import TextareaAutosize from 'react-textarea-autosize';
+
+
 const Input = () => {
 	const globalInput = useContentStore((state) => state.globalInput);
 	const setGlobalInput = useContentStore((state) => state.setGlobalInput);
@@ -27,11 +30,12 @@ const Input = () => {
 	return (
 		<section className="flex flex-col items-center justify-center my-20 bg-white md:w-4/5 ">
 			<div className="flex items-center w-full mx-auto md:w-3/5">
-				<div className="flex-1 relative px-4 h-[100px] py-2 rounded-xl border shadow-md border-primary">
-					<input
+				<div className="relative flex-1 px-4 py-2 border shadow-md rounded-xl border-primary">
+					<TextareaAutosize
 						placeholder="Ask anything..."
 						rows={1}
-						className="w-full h-10 p-3 overflow-auto text-xl font-medium focus:outline-none"
+						maxRows={20}
+						className="w-full p-3 overflow-auto text-xl font-medium resize-none focus:outline-none"
 						value={globalInput}
 						onKeyDown={(e) => handleKeyDown(e)}
 						onChange={(e) => setGlobalInput(e.target.value)}
